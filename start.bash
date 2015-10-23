8d7a71ffc527849cc1fb772e7a798724fd4be776
Benjamín Martínez Mateos <xaamin@outlook.com>
2015-10-22 19:37:34 -0500

    Removed bad code that try generate SSL certificate

--- start.bash
+++ start.bash
@@ -20,12 +20,6 @@ if [[ -d "$OVERRIDE/$SITES" ]]; then
   ln -s "$OVERRIDE/$SITES" "$ENABLED"
 fi
 
-# Create SSL directory and SSL certificate for securing Nginx
-if [[ ! -d "$OVERRIDE/$SSL" ]]; then
-	mkdir -p "$OVERRIDE/$SSL"
-	/usr/bin/openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout "$OVERRIDE/$SSL/nginx.key" -out "$OVERRIDE/$SSL/nginx.crt"
-fi
-
 # Create logs directory
 if [[ ! -d "$OVERRIDE/$LOGS" ]]; then
 	mkdir -p "$OVERRIDE/$LOGS"
