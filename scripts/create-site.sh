deef051b19e3137e763f595c3d8a70c4ba4bb16c
Benjamín Martínez Mateos <xaamin@outlook.com>
2018-04-04 10:42:17 -0500

    Update scritp paths

--- scripts/create-site.sh
+++ scripts/create-site.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SHARED="shared"
+SHARED="shared/server"
 
 echo "This removes previous certificates if exist. Do you want continue. (Y/N)? "
 
@@ -12,12 +12,12 @@ if [[ $ANSWER =~ ^[Yy]$ ]]; then
 
     read ACCOUNT
 
-    if [[ ! -d "$SHARED/$ACCOUNT" ]]; then
+    if [[ ! -d "$SHARED/web/$ACCOUNT" ]]; then
         echo "Creating account path and site config..."
         echo ""
-        mkdir -p "$SHARED/$ACCOUNT"
+        mkdir -p "$SHARED/web/$ACCOUNT"
 
-        cp -rf "$SHARED/templates/site/." "$SHARED/$ACCOUNT/"
+        cp -rf "$SHARED/templates/site/." "$SHARED/web/$ACCOUNT/"
 
         cp -f "$SHARED/templates/site.conf" "$SHARED/sites/$ACCOUNT"
 
@@ -25,21 +25,21 @@ if [[ $ANSWER =~ ^[Yy]$ ]]; then
     fi
 
     # Delete SSL directory if dir exist
-    if [[ -d "$SHARED/$ACCOUNT/ssl" ]]; then
+    if [[ -d "$SHARED/web/$ACCOUNT/ssl" ]]; then
         echo "Removing old SSL path..."
         echo ""
-        rm -rf "$SHARED/$ACCOUNT/ssl"
+        rm -rf "$SHARED/web/$ACCOUNT/ssl"
     fi
 
     # Create SSL directory and certificate for securing Nginx
-    if [[ ! -d "$SHARED/$ACCOUNT/ssl" ]]; then
+    if [[ ! -d "$SHARED/web/$ACCOUNT/ssl" ]]; then
         echo "Creating SSL dir..."
         echo ""
-        mkdir -p "$SHARED/$ACCOUNT/ssl"
+        mkdir -p "$SHARED/web/$ACCOUNT/ssl"
 
         echo "Creating $ACCOUNT certificates..."
         echo ""
-        /usr/bin/openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout "$SHARED/$ACCOUNT/ssl/nginx.key" -out "$SHARED/$ACCOUNT/ssl/nginx.crt"
+        /usr/bin/openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout "$SHARED/web/$ACCOUNT/ssl/nginx.key" -out "$SHARED/web/$ACCOUNT/ssl/nginx.crt"
     fi
 
     echo ""
