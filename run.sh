42c14b356fcf13ec78ddbecc2f23877afa03baa4
Benjamín Martínez Mateos <xaamin@outlook.com>
2019-04-17 13:39:06 -0500

    Custom password and new docker compose env generator

--- run.sh
+++ run.sh
@@ -1,8 +1,37 @@
 #!/bin/bash
 export DOCKER_USER=${DOCKER_USER:-'xaamin'}
 
+export USER=$DOCKER_USER
+
+if [ -f '.mysql.env' ]; then
+    export MYSQL_ROOT_PASSWORD=$(cat .mysql.env)
+else
+    export MYSQL_ROOT_PASSWORD=$(openssl rand -base64 32)
+
+    echo $MYSQL_ROOT_PASSWORD > .mysql.env
+fi
+
+if [ -f '.ssh.env' ]; then
+    export SSH_PASSWORD=$(cat .ssh.env)
+else
+    export SSH_PASSWORD=$(openssl rand -base64 32)
+
+    echo $SSH_PASSWORD > .ssh.env
+fi
+
 export SHARED_VOLUME="/home/$DOCKER_USER"
 
-echo "Using path '$SHARED_VOLUME' for shared volume"
+export USER_ID=$(id -u $(whoami))
 
-export USER_ID=$(id -u $(whoami)) && export GROUP_ID=$(id -g $(whoami)) && docker-compose up -d
+export USER_GID=$(id -g $(whoami))
+
+echo ""
+echo "========================================="
+echo "Using shared volume: '$SHARED_VOLUME'"
+echo "MySQL password: '$MYSQL_ROOT_PASSWORD'"
+echo "SSH password: '$SSH_PASSWORD'"
+echo "Creating as user $USER with UID $USER_ID and GUID $USER_GID"
+echo "========================================="
+echo ""
+
+ docker-compose up -d
