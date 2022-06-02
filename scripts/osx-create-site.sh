ba358a96f25dc527706ec39c9f24f88fa87f73ec
Benjamín Martínez Mateos <xaamin@outlook.com>
2022-06-02 18:29:31 -0500

    Minor improvements to allow create sites in a pretty way

--- scripts/osx-create-site.sh
+++ scripts/osx-create-site.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 SHARED="$(pwd)/shared"
-DOCKER_USER=${DOCKER_USER:-'xaamin'}
-SERVER_PATH="/home/$DOCKER_USER"
+DOCKER_USER=${DOCKER_USER:-'rocket'}
+SERVER_PATH="$SHARED"
 
 echo "This removes previous certificates if exists. Do you want to continue. (Y/N)? "
 
@@ -10,11 +10,11 @@ read ANSWER
 
 if [[ $ANSWER =~ ^[Yy]$ ]]; then
 
-    echo "Provide an account name: "
+    echo "Provide the project name: "
 
-    read ACCOUNT
+    read PROJECT
 
-    if [[ -d "$SHARED/web/$ACCOUNT" ]]; then
+    if [[ -d "$SHARED/web/$PROJECT" ]]; then
         echo "Source files found. Do you want to remove them. (Y/N)? "
 
         read ANSWER
@@ -22,30 +22,37 @@ if [[ $ANSWER =~ ^[Yy]$ ]]; then
         if [[ $ANSWER =~ ^[Yy]$ ]]; then
             echo "Removing source files..."
 
-            rm -rf "$SHARED/web/$ACCOUNT"
+            rm -rf "$SHARED/web/$PROJECT"
         fi
     fi
 
-    if [[ ! -d "$SHARED/web/$ACCOUNT" ]]; then
+    echo "Provide a domain name: "
+
+    read DOMAIN_NAME
+
+    if [[ ! -d "$SHARED/web/$PROJECT" ]]; then
         echo "Creating conf path..."
-        mkdir -p "$SHARED/server/conf/$ACCOUNT"
+        mkdir -p "$SHARED/server/conf/$PROJECT"
 
         echo "Creating log path..."
-        mkdir -p "$SHARED/server/log/$ACCOUNT"
+        mkdir -p "$SHARED/server/log/$PROJECT"
 
-        echo "Creating account path and site config..."
-        mkdir -p "$SHARED/web/$ACCOUNT"
+        echo "Creating PROJECT path and site config..."
+        mkdir -p "$SHARED/web/$PROJECT"
 
-        cp -rf "$SHARED/server/templates/site/conf/" "$SHARED/server/conf/$ACCOUNT/"
+        cp -rf "$SHARED/server/templates/site/conf/" "$SHARED/server/conf/$PROJECT/"
 
-        cp -f "$SHARED/server/templates/site.conf" "$SHARED/server/sites/$ACCOUNT"
+        cp -f "$SHARED/server/templates/site.conf" "$SHARED/server/sites/$DOMAIN_NAME"
 
-        sed -i '' 's|root .*|root '${SERVER_PATH}'/web/'$ACCOUNT';|' "$SHARED/server/sites/$ACCOUNT" || true
-        sed -i '' 's|ssl_certificate .*|ssl_certificate '${SERVER_PATH}'/server/ssl/'$ACCOUNT'/nginx.crt;|' "$SHARED/server/sites/$ACCOUNT" || true
-        sed -i '' 's|ssl_certificate_key .*|ssl_certificate_key '${SERVER_PATH}'/server/ssl/'$ACCOUNT'/nginx.key;|' "$SHARED/server/sites/$ACCOUNT" || true
+        sed -i '' 's|root .*|root '${SERVER_PATH}'/web/'$PROJECT';|' "$SHARED/server/sites/$DOMAIN_NAME" || true
+        sed -i '' 's|ssl_certificate .*|ssl_certificate '${SERVER_PATH}'/server/ssl/'$PROJECT'/nginx.crt;|' "$SHARED/server/sites/$DOMAIN_NAME" || true
+        sed -i '' 's|ssl_certificate_key .*|ssl_certificate_key '${SERVER_PATH}'/server/ssl/'$PROJECT'/nginx.key;|' "$SHARED/server/sites/$DOMAIN_NAME" || true
 
         # Change example.test to right host
-        sed -i '' "s|example.test|$ACCOUNT|g" "$SHARED/server/sites/$ACCOUNT"
+        sed -i '' "s|example.test|$DOMAIN_NAME|g" "$SHARED/server/sites/$DOMAIN_NAME"
+
+        # Update upstream
+        sed -i '' "s|fastcgi_pass .*|fastcgi_pass $PROJECT:9000;|g" "$SHARED/server/sites/$DOMAIN_NAME"
 
         echo "Do you want clone some repository into the site document root. (Y/N)? "
 
@@ -56,32 +63,32 @@ if [[ $ANSWER =~ ^[Yy]$ ]]; then
 
             read REPOSITORY
 
-            git clone $REPOSITORY "$SHARED/web/$ACCOUNT/"
+            git clone $REPOSITORY "$SHARED/web/$PROJECT/"
 
         else
-            cp -rf "$SHARED/server/templates/site/www/" "$SHARED/web/$ACCOUNT/"
+            cp -rf "$SHARED/server/templates/site/www/" "$SHARED/web/$PROJECT/"
         fi
     fi
 
     # Delete SSL directory if dir exist
-    if [[ -d "$SHARED/server/ssl/$ACCOUNT" ]]; then
+    if [[ -d "$SHARED/server/ssl/$PROJECT" ]]; then
         echo "Removing old SSL path..."
-        rm -rf "$SHARED/server/ssl/$ACCOUNT"
+        rm -rf "$SHARED/server/ssl/$PROJECT"
     fi
 
     # Create SSL directory and certificate for securing Nginx
-    if [[ ! -d "$SHARED/server/ssl/$ACCOUNT" ]]; then
+    if [[ ! -d "$SHARED/server/ssl/$PROJECT" ]]; then
         echo "Creating SSL dir..."
-        mkdir -p "$SHARED/server/ssl/$ACCOUNT"
+        mkdir -p "$SHARED/server/ssl/$PROJECT"
 
-        echo "Creating $ACCOUNT certificates..."
-        /usr/bin/openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout "$SHARED/server/ssl/$ACCOUNT/nginx.key" -out "$SHARED/server/ssl/$ACCOUNT/nginx.crt"
+        echo "Creating $PROJECT certificates..."
+        /usr/bin/openssl req -x509 -nodes -days 365 -newkey rsa:2048 -subj "/C=MX/ST=Oaxaca/L=México/O=Global Fintech/OU=IT Department/CN=$DOMAIN_NAME" -keyout "$SHARED/server/ssl/$PROJECT/nginx.key" -out "$SHARED/server/ssl/$PROJECT/nginx.crt"
     fi
 
     # Create log files
-    if [[ ! -d "$SHARED/server/log/$ACCOUNT" ]]; then
+    if [[ ! -d "$SHARED/server/log/$PROJECT" ]]; then
         echo "Creating log dir..."
-        mkdir -p "$SHARED/server/log/$ACCOUNT"
+        mkdir -p "$SHARED/server/log/$PROJECT"
 
     fi
 
